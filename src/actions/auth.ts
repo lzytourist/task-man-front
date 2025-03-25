7a72087c8587110add4d5d8fc51621cd3f0dbfbd
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-25 12:59:29 +0600

    Match token expiration with server

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -2,6 +2,7 @@
 
 import {LoginSchemaType} from "@/types";
 import {cookies} from "next/headers";
+import jwt from "jsonwebtoken";
 
 interface LoginToken {
   access: string;
@@ -10,6 +11,11 @@ interface LoginToken {
 
 const ACCESS_COOKIE_NAME = 'access';
 const REFRESH_COOKIE_NAME = 'refresh';
+
+const getTokenExpiry = (token: string) => {
+  const {exp} = jwt.decode(token) as {exp: number}
+  return exp;
+};
 
 export const login = async (data: LoginSchemaType) => {
   const response = await fetch(`${process.env.API_URL}/account/token/`, {
@@ -25,8 +31,18 @@ export const login = async (data: LoginSchemaType) => {
   if (response.ok) {
     const data = await response.json() as LoginToken;
     const cookieStore = await cookies();
-    cookieStore.set(ACCESS_COOKIE_NAME, data.access);
-    cookieStore.set(REFRESH_COOKIE_NAME, data.refresh);
+
+    const accessExp = getTokenExpiry(data.access);
+    const refreshExp = getTokenExpiry(data.refresh);
+
+    cookieStore.set(ACCESS_COOKIE_NAME, data.access, {
+      httpOnly: true,
+      expires: accessExp * 1000
+    });
+    cookieStore.set(REFRESH_COOKIE_NAME, data.refresh, {
+      httpOnly: true,
+      expires: refreshExp * 1000
+    });
     return {
       error: false,
       data: data
