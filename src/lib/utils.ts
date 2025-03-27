d1eb13fe0a5dfdafbddce91105635d4bd9e2bda3
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-03-27 23:38:07 +0600

    Update dateformat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,29 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
 import jwt from "jsonwebtoken";
-import {AuthUserType, UserType} from "@/types";
+import {AuthUserType} from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const getTokenExpiry = (token: string) => {
-  const {exp} = jwt.decode(token) as {exp: number}
+  const {exp} = jwt.decode(token) as { exp: number }
   return exp;
 };
 
-export const hasPermission = (required: string[], user: AuthUserType)=> {
+export const hasPermission = (required: string[], user: AuthUserType) => {
   const userPermissions = user.role.permissions_list.map(perm => perm.codename);
   return required.every(perm => userPermissions.includes(perm));
 }
 
-export const formatDate = (date: string) => {
-  return Intl.DateTimeFormat("en-BD", {
+export const formatDate = (date: string | null, dateOnly: boolean = false) => {
+  if (!date) return '';
+
+  const options: { [key: string]: string } = {
     year: "numeric",
     month: "short",
     day: "2-digit",
-    weekday: "short",
-    hour: "2-digit",
-    minute: "2-digit"
-  }).format(new Date(date));
+  };
+
+  if (dateOnly) {
+    options['weekday'] = 'short';
+    options['hour'] = '2-digit';
+    options['minute'] = '2-digit';
+  }
+
+  return Intl.DateTimeFormat("en-BD", options).format(new Date(date));
 }
