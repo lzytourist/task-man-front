ed977ad3488996fedef1f17e907369578a0f795d
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-04 16:53:51 +0600

    Create task button

--- src/components/dashboard/nav-main.tsx
+++ src/components/dashboard/nav-main.tsx
@@ -31,11 +31,14 @@ export function NavMain({items,}: {
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
+              asChild={true}
               tooltip="Quick Create"
               className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
             >
-              <PlusCircleIcon/>
-              <span>Quick Create</span>
+              <Link href={'/dashboard/tasks/create'}>
+                <PlusCircleIcon/>
+                <span>Quick Create</span>
+              </Link>
             </SidebarMenuButton>
 
             <Notifications/>
