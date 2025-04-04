13122d6d7bc8ab098f80e0a04582a34ecc134f36
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-04-04 12:12:50 +0600

    Task table design

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -7,6 +7,14 @@ import ActionButtons from "@/app/dashboard/tasks/action-buttons";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import {PlusIcon} from "lucide-react";
+import {Badge} from "@/components/ui/badge";
+
+const statusColor = {
+  'pending': 'text-purple-500',
+  'accepted': 'text-orange-500',
+  'in_progress': 'text-yellow-400',
+  'completed': 'text-green-500'
+};
 
 export default async function Page() {
   const tasks = await getTasks() as Task[];
@@ -44,8 +52,11 @@ export default async function Page() {
                 <TableCell>{task.created_by.name}</TableCell>
                 <TableCell>{task.assigned_to?.name ?? 'N/A'}</TableCell>
                 <TableCell>{formatDate(task.deadline, true)}</TableCell>
-                <TableCell>{task.priority}</TableCell>
-                <TableCell>{task.status}</TableCell>
+                <TableCell>
+                  <Badge variant={['high', 'critical'].includes(task.priority) ? 'destructive' : 'default'}>{task.priority}</Badge>
+                </TableCell>
+                <TableCell
+                  className={statusColor[task.status as 'pending' | 'completed' | 'in_progress' | 'accepted']}>{task.status}</TableCell>
                 <TableCell>
                   <ActionButtons id={task.id}/>
                 </TableCell>
